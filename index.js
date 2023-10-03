f629be98d3bc71953759310b048ab232f4f07678
Jamkris <dltmdgus1412@gmail.com>
2023-10-03 15:05:47 +0900

    EDIT :: CORS

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(
 	cors({
-		origin: '*',
+		origin: 'http://localhost:3000',
 		credentials: true,
 		withCredentials: true,
 		optionsSuccessStatus: 200,
