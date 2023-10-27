48b8bff86e82aeafeec8b1e6e5046b717ad97a52
Jamkris <dltmdgus1412@gmail.com>
2023-10-27 21:07:22 +0900

    EDIT :: Origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
 	cors({
-		origin: 'http://localhost:3000/',
+		origin: process.env.ORIGIN,
 		credentials: true,
 		withCredentials: true,
 		optionsSuccessStatus: 200,
